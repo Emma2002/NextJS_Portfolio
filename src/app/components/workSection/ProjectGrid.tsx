aac312000ac6a86957f6422e97c8512b4ed97779
EmmavandenBroek <e.m.vandenbroek@student.utwente.nl>
2023-12-04 10:43:16 +0100

    Fixed project card bug

--- src/app/components/workSection/ProjectGrid.tsx
+++ src/app/components/workSection/ProjectGrid.tsx
@@ -1,18 +1,20 @@
+"use client"
 import ProjectCard from "./ProjectCard";
 import { universityProjects, designProjects, ProjectProps } from "./ProjectDetails";
 import { useState } from "react";
 
 const ProjectGrid = () => {
-  const [filter, setFilter] = useState(true);
+  // const [filter, setFilter] = useState(true);
+  const [filter, setFilter] = useState("university");
 
   return (
     <>
       <div className="mb-10 flex justify-center gap-16 text-[#e4ded7] md:mb-16 lg:mb-20  ">
         <h4
           className={`text-[16px] md:text-[20px] lg:text-[24px] ${
-            filter ? "text-[#e4ded7]" : "text-[#e4ded7]/30"
+            filter === "university" ? "text-[#e4ded7]" : "text-[#e4ded7]/30"
           }`}
-          onClick={() => setFilter(true)}
+          onClick={() => setFilter("university")}
         >
           University Projects
         </h4>{" "}
@@ -20,13 +22,27 @@ const ProjectGrid = () => {
           className={`text-[16px] md:text-[20px] lg:text-[24px] ${
             filter ? "text-[#e4ded7]/30" : "text-[#e4ded7]"
           }`}
-          onClick={() => setFilter(false)}
+          onClick={() => setFilter("design")}
         >
           Design
         </h4>
       </div>
 
-      {filter ? (
+      
+      <div className="flex justify-center items-center h-full">
+        <div className="grid w-[80%] grid-cols-1 grid-rows-2 gap-y-10 gap-x-6 lg:max-w-[1200px] lg:grid-cols-1">
+          {filter === "university" &&
+            universityProjects.map((project: ProjectProps) => (
+              <ProjectCard key={project.id} {...project} />
+            ))}
+          {filter === "design" &&
+            designProjects.map((project: ProjectProps) => (
+              <ProjectCard key={project.id} {...project} />
+            ))}
+        </div>
+      </div>
+  
+      {/* {filter ? (
         <div className="flex justify-center items-center h-full">
           <div className="grid w-[80%] grid-cols-1 grid-rows-2 gap-y-10 gap-x-6 lg:max-w-[1200px] lg:grid-cols-1">
             {universityProjects.map((project: ProjectProps) => (
@@ -48,7 +64,7 @@ const ProjectGrid = () => {
           ))}
         </div>
         </div>
-      )}
+      )} */}
     </>
   );
 };
