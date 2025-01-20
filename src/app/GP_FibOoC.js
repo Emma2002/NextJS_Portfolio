b4d095f2352b37e3726126986715422b575049bf
Emma <emmamyrthe@gmail.com>
2025-01-20 22:24:18 +0100

    Added page for GP

--- src/app/GP_FibOoC.js
+++
@@ -1,9 +0,0 @@
-
-export default function GP_FibOoC() {
-    return (
-        <div>
-            <h1>Welcome to GP_FibOoC</h1>
-            <p>This is your custom Next.js page for the route GP_FibOoC.</p>
-        </div>
-    );
-}
---
+++ src/app/GP_FibOoC/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const GP_FibOoC = () => {
+  return (
+    <div>
+      <h1>Graduation Project FibOoC</h1>
+      <p>This is a test page for the Unity game. The game will appear here later!</p>
+    </div>
+  );
+};
+
+export default GP_FibOoC;
