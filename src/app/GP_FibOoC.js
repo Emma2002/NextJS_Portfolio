38ea6765b6e73f2660c079a5c0720857a768b7a1
Emma <emmamyrthe@gmail.com>
2025-01-20 22:14:50 +0100

    Create GP_FibOoC.js

---
+++ src/app/GP_FibOoC.js
@@ -0,0 +1,9 @@
+
+export default function GP_FibOoC() {
+    return (
+        <div>
+            <h1>Welcome to GP_FibOoC</h1>
+            <p>This is your custom Next.js page for the route GP_FibOoC.</p>
+        </div>
+    );
+}
